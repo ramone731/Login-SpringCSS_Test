4814037b1eb906edc8d05a6d4479a158bf86c466
rl <74465003+ramone731@users.noreply.github.com>
2024-07-20 21:49:20 -0400

    Add files via upload

---
+++ SpringCssV2/src/main/java/org/example/rl/springcssv2/SpringCssV2Application.java
@@ -0,0 +1,13 @@
+package org.example.rl.springcssv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCssV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCssV2Application.class, args);
+    }
+
+}
